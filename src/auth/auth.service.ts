5789f05b8ac33f96895fa7991a2d41c6be4bcd76
tuannh-2670 <nguyen.huu.tuan-b@sun-asterisk.com>
2024-03-12 22:31:23 +0700

    init project & create module, controller, service

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@nestjs/common";
+import { User, Note } from "@prisma/client";
+import { PrismaService } from "../prisma/prisma.service";
+
+@Injectable({})
+export class AuthService {
+    constructor(private prismaService: PrismaService) {
+
+    }
+
+    register() {
+        return {
+            message: "Register an user"
+        }
+    }
+
+    login() {
+        return {
+            message: "this is login"
+        }
+    }
+}
