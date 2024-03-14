ddbb9ef1b6647d4c306b34a02b04c357e83131f8
tuannh-2670 <nguyen.huu.tuan-b@sun-asterisk.com>
2024-03-14 22:45:55 +0700

    custom decorator & unit test with pactum

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express'
+import { MyJwtGuard } from '../auth/guard';
+import { GetUser } from '../auth/decorator';
+import { User } from '@prisma/client';
 
 @Controller('users')
 export class UserController {
-    @UseGuards(AuthGuard('jwt'))
+    @UseGuards(MyJwtGuard)
     @Get('me')
-    me(@Req() request: Request) {
-        return request.user
+    me(@GetUser() user: User) {
+        return user
     }
 }
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,102 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { Test } from "@nestjs/testing";
+import { AppModule } from "../src/app.module";
+import { INestApplication, ValidationPipe } from "@nestjs/common";
+import { PrismaService } from "../src/prisma/prisma.service";
+import * as pactum from 'pactum';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
+const PORT = 3002
+describe('App EndToEnd tests', () => {
+  let app: INestApplication
+  let prismaService: PrismaService
+  beforeAll(async () => {
+    const appModule = await Test.createTestingModule({
+      imports: [AppModule]
+    }).compile()
+    app = appModule.createNestApplication()
+    app.useGlobalPipes(new ValidationPipe())
+    await app.init()
+    await app.listen(PORT)
+    prismaService = app.get(PrismaService)
+    await prismaService.cleanDatabase()
+    pactum.request.setBaseUrl(`http://localhost:${PORT}`)
+  })
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+  describe('Test Authentication', () => {
+    describe('Register', () => {
+      it('should register successfully', () => {
+        return pactum.spec()
+          .post('/auth/register')
+          .withBody({
+            email: 'testemail@example.com',
+            password: '123456'
+          })
+          .expectStatus(201)
+        // .inspect()
+      })
+      it('should show error with wrong format email', () => {
+        return pactum.spec()
+          .post('/auth/register')
+          .withBody({
+            email: 'abc',
+            password: '123456'
+          })
+          .expectStatus(400)
+      })
+      it('should show error with empty email', () => {
+        return pactum.spec()
+          .post('/auth/register')
+          .withBody({
+            email: '',
+            password: '123456'
+          })
+          .expectStatus(400)
+      })
+      it('should show error with empty password', () => {
+        return pactum.spec()
+          .post('/auth/register')
+          .withBody({
+            email: 'testemail@example.com',
+            password: ''
+          })
+          .expectStatus(400)
+      })
+    })
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+    describe('Login', () => {
+      it('should login successfully', () => {
+        return pactum.spec()
+          .post('/auth/login')
+          .withBody({
+            email: 'testemail@example.com',
+            password: '123456'
+          })
+          .expectStatus(201)
+          .stores('accessToken', "accessToken")
+      })
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
-});
+      it('should login failed', () => {
+        return pactum.spec()
+          .post('/auth/login')
+          .withBody({
+            email: 'testemail@example.com',
+            password: '126'
+          })
+          .expectStatus(403)
+      })
+    })
+
+    describe('Get detail user', () => {
+      it('should get user info successfully', () => {
+        return pactum.spec()
+          .get('/users/me')
+          .withBearerToken('$S{accessToken}')
+          .expectStatus(200)
+      })
+    })
+  })
+
+  afterAll(async () => {
+    app.close()
+  })
+  it.todo('should PASS, haha');
+})
